56a32e7aacc8eddb48633fc71d524b39a0195b62
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2025-12-08 20:59:26 -0600

    cleaning up the header

--- src/comps/header/Header.tsx
+++ src/comps/header/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
 		<div className="header">
 			<ThemeToggle />
 			<h1 className="header-title">
-				{theme === 'dark' ? 'zZThe Dark OracleZz' : 'zZThe Light OracleZz'}
+				{theme === 'dark' ? 'Darkness' : 'Lightness'}
 			</h1>
 			<div className="header-glow"></div>
 		</div>
